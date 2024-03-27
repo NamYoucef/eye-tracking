e3bdef1f697e3c38db83ed811ab24dc188ef6462
Namoun Youcef <youcef.namoun@imadrassa.com>
2024-03-27 10:43:06 +0100

    refactoring

--- src/DrawingCanvas.jsx
+++ src/DrawingCanvas.jsx
@@ -1,10 +1,13 @@
 import React, { useRef, useState, useEffect } from "react";
-import { Button } from 'antd';
-import { CloseCircleOutlined } from '@ant-design/icons';
-import Timer from './Timer';
-import labyrinthe_1 from './assets/labyrinthe_1.jpeg';
-import labyrinthe_2 from './assets/labyrinthe_2.jpeg';
-import labyrinthe_3 from './assets/labyrinthe_3.png';
+import { Button, Badge } from 'antd';
+import { CloseCircleOutlined, ForwardOutlined, RedoOutlined } from '@ant-design/icons';
+import Tentative from './Tentative';
+import labyrinthe_1 from './assets/labyrinthe1.png';
+import labyrinthe_2 from './assets/labyrinthe2.png';
+import labyrinthe_3 from './assets/labyrinthe3.png';
+import labyrinthe_4 from './assets/labyrinthe4.png';
+import labyrinthe_5 from './assets/labyrinthe5.png';
+import labyrinthe_6 from './assets/labyrinthe6.png';
 import "./index.css";
 
 
@@ -63,19 +66,27 @@ const DrawingCanvas = () => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
         const image = new Image();
-        if (value) {
-            setActive(value)
-            if (value === 1) image.src = labyrinthe_1;
-            if (value === 2) image.src = labyrinthe_2;
-            if (value === 3) image.src = labyrinthe_3;
-        } else {
-            if (active === 1) image.src = labyrinthe_1;
-            if (active === 2) image.src = labyrinthe_2;
-            if (active === 3) image.src = labyrinthe_3;
+        if (active && active <= 6) {
+            if (value) {
+                if (active === 1) image.src = labyrinthe_1;
+                if (active === 2) image.src = labyrinthe_2;
+                if (active === 3) image.src = labyrinthe_3;
+                if (active === 4) image.src = labyrinthe_4;
+                if (active === 5) image.src = labyrinthe_5;
+                if (active === 6) image.src = labyrinthe_6;
+            } else {
+                setActive(active + 1)
+                if (active === 1) image.src = labyrinthe_1;
+                if (active === 2) image.src = labyrinthe_2;
+                if (active === 3) image.src = labyrinthe_3;
+                if (active === 4) image.src = labyrinthe_4;
+                if (active === 5) image.src = labyrinthe_5;
+                if (active === 6) image.src = labyrinthe_6;
+            }
+            image.onload = () => {
+                context.drawImage(image, 0, 0, canvas.width, canvas.height);
+            };
         }
-        image.onload = () => {
-            context.drawImage(image, 0, 0, canvas.width, canvas.height);
-        };
     };
 
 
@@ -84,35 +95,40 @@ const DrawingCanvas = () => {
         const context = canvas.getContext("2d");
         context.clearRect(0, 0, canvas.width, canvas.height);
         setNbError(nbError + 1)
-        loadImage()
+        console.log('active', active)
+        loadImage(active - 1)
     };
+
+    const restart = () => {
+        clearCanvas()
+        setActive(0)
+    }
 
 
     return (
         <div className="container">
             <div className="title">
-                Nombre de tentative: <span style={{ color: "red", fontWeight: 550 }}>{nbError}</span>
+                Labyrinthe: <span style={{ color: "red", fontWeight: 550 }}>{active - 1}/6</span>
             </div>
             <div style={{ display: 'flex' }}>
                 <div className="lab_left_buttons">
-                    <Button key={1} className="button" onClick={() => loadImage(1)} type="primary">Labyrinthe 1</Button>
-                    <Button key={2} className="button" onClick={() => loadImage(2)} type="primary">Labyrinthe 2</Button>
-                    <Button key={3} className="button" onClick={() => loadImage(3)} type="primary">Labyrinthe 3</Button>
+                    <Badge count={nbError || 0} className="button">
+                        <Button key={2} icon={<CloseCircleOutlined />} onClick={clearCanvas} type="primary">Clean</Button>
+                    </Badge>
+                    <Button icon={<ForwardOutlined />} danger onClick={() => loadImage()} type="primary" style={{ marginTop: 20, fontWeight: 500, marginRight: 40 }}>Next</Button>
+
                 </div>
                 <canvas
                     ref={canvasRef}
-                    width={1000}
-                    height={600}
+                    width={1100}
+                    height={650}
                     className="canvas"
                 >
                     <img src={labyrinthe_1} alt="" />
                 </canvas>
 
-                <Timer />
-
             </div>
             <div>
-                <Button icon={<CloseCircleOutlined />} onClick={clearCanvas} type="primary" style={{ marginTop: 20 }}>Effacer</Button>
             </div>
         </div>
     );
---
+++ src/Tentative.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./index.css";
+import { Badge, Button } from 'antd';
+import { CloseCircleOutlined } from '@ant-design/icons';
+
+const Tentative = ({ nbError }) => {
+
+    return (
+        <div>
+            <div className='timer'>
+            </div>
+        </div>
+    );
+};
+
+export default Tentative;
